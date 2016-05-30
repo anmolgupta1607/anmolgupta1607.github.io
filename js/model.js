2b0278b8a0c863c55dda3439cc53dd12c8a32076
anmolgupta <anmol.gupta@snapdeal.com>
2016-05-30 18:10:29 +0530

    ADSUI-768:new content

---
+++ js/model.js
@@ -0,0 +1,86 @@
+ var populate =  [{
+	"step_url": "getting-started-with-snapdeal-ads",
+	"step_name": "Getting Started with Snapdeal Ads",
+	"step_id": 0,
+	"step_content": "getting-started-with-snapdeal-ads.html",
+	"step-arrow": "getting-started-with-snapdeal-ads-arrow",
+	"step-border": "getting-started-with-snapdeal-ads-border",
+	"documents": []
+}, {
+	"step_url": "create-campaign",
+	"step_name": "Create campaign",
+	"step_id": 1,
+	"step_content": "create-campaign.html",
+	"step-arrow": "create-campaign-arrow",
+	"step-border": "create-campaign-border",
+	"documents": [{
+		"document_name": "Create campaign",
+		"document_id": "create-campaign",
+		"document_url": "/videos.html#create-campaign-v2"
+	}]
+}, {
+	"step_url": "view-campaign",
+	"step_name": "View and manage campaigns",
+	"step_id": 2,
+	"step_content": "view-campaign.html",
+	"step-arrow": "view-campaign-arrow",
+	"step-border": "view-campaign-border",
+	"documents": [{
+		"document_name": "View and manage campaigns",
+		"document_id": "view-campaign",
+		"document_url": "/videos.html#view-and-manage-campaign-v2"
+	}]
+},  {
+	"step_url": "add-funds-to-your-account",
+	"step_name": "Adding Funds to your Ad Account",
+	"step_id": 4,
+	"step_content": "add-funds-to-your-account.html",
+	"step-arrow": "add-funds-to-your-account-arrow",
+	"step-border": "add-funds-to-your-account-border",
+	"documents": [{
+		"document_name": "Add Funds and transaction history",
+		"document_id": "add-funds",
+		"document_url": "/videos.html#add-funds-and-view-transactions-advance"
+	}]
+}, {
+	"step_url": "report",
+	"step_name": "Campaign reports",
+	"step_id": 5,
+	"step_content": "report.html",
+	"step-arrow": "report-arrow",
+	"step-border": "report-border",
+	"documents": [{
+		"document_name": "Campaign reports",
+		"document_id": "campaign-report",
+		"document_url": "/videos.html#campaign-reports-v2"
+	}]
+}];
+
+var populate_faq = [{
+	"step_url": "ad-serving",
+	"step_name": "Ad serving",
+	"step_id": 0,
+	"step_content": "ad-serving.html"
+}, {
+	"step_url": "campaign-creation",
+	"step_name": "Campaign creation",
+	"step_id": 1,
+	"step_content": "campaign-creation.html"
+}, {
+	"step_url": "campaign-state",
+	"step_name": "Campaign state management",
+	"step_id": 2,
+	"step_content": "campaign-state.html"
+}, {
+
+	"step_url": "campaign-report",
+	"step_name": "Campaign reports",
+	"step_id": 3,
+	"step_content": "campaign-report.html"
+}, {
+
+	"step_url": "add-funds",
+	"step_name": "Add funds and transaction history",
+	"step_id": 4,
+	"step_content": "add-funds.html"
+}];
